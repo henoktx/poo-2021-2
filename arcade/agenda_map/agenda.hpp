3e63f82130b3e8a5b9eb7b6bd2dc75ccb83bd6c6
Henok <henokteixeira@gmail.com>
2021-12-03 00:40:17 -0300

    add agenda feita com map

---
+++ arcade/agenda_map/agenda.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <algorithm>
+#include <list>
+#include <map>
+#include "../contatos/contato.hpp"
+
+class Agenda {
+private:
+    std::map<std::string, std::shared_ptr<Contato>> contatos {};
+
+    int procuraPos(const std::string &nome) {
+        int contador {0};
+        for (auto i : this->contatos) {
+            if (nome == i.second->getName()) {
+                return contador;
+            }
+            contador++;
+        }
+        return -1;
+    }
+
+public:
+    Agenda() {}
+
+    void addContato(std::shared_ptr<Contato> &contato) {
+        for (auto it = contatos.begin(); it != contatos.end(); it++) {
+            if (contato->getName() == it->second->getName()) {
+                for (auto i : contato->getFones()) {
+                    it->second->addFone(i);
+                    std::cout << "Contato adicionado com sucesso" << std::endl;
+                    return;
+                }
+            } 
+        }
+        contatos.insert(std::make_pair(contato->getName(), contato));
+        std::cout << "Contato adicionado com sucesso" << std::endl;
+    }
+
+    void rmContato(const std::string &nome) {
+        for (auto it = contatos.begin(); it != contatos.end(); it++) {
+            if (nome == it->second->getName()) {
+                contatos.erase(it);
+                std::cout << "Contato removido com sucesso" << std::endl;
+                return;
+            }
+        } 
+        std::cout << nome << " não corresponde a nenhum contato da agenda" << std::endl; 
+    }
+    
+    std::shared_ptr<Contato> getContato(const std::string &nome) {     
+        std::list<std::shared_ptr<Fone>> vazia;
+        auto vazio = std::make_shared<Contato>(" ", vazia);
+        for (auto it = contatos.begin(); it != contatos.end(); it++) {
+            if (nome == it->second->getName()) {
+                std::cout << "Contato encontrado: " << it->second->toString() << std::endl;
+                return it->second;                
+            }
+        }        
+        std::cout << nome << " não corresponde a nenhum contato da agenda" << std::endl;
+        return vazio;
+    }
+
+    std::list<std::shared_ptr<Contato>> procura(const std::string &pattern) {
+        std::list<std::shared_ptr<Contato>> encontrados {};
+        for (auto it = contatos.begin(); it != contatos.end(); it++) {
+            std::string parte = it->second->getName();
+            if (parte.find(pattern) != std::string::npos) {
+                encontrados.push_back(it->second);
+                std::cout << it->second->toString() << std::endl;
+            }
+        }
+        if ((int) encontrados.size() <= 0) {
+            if ((int) contatos.size() <= 0) {
+                std::cout << "Não há contatos na agenda" << std::endl;
+                return encontrados;
+            } 
+            std::cout << "Nenhuma correspondência a " << pattern << std::endl;
+        }
+        return encontrados;
+    }
+
+    std::map<std::string, std::shared_ptr<Contato>> getContatos() {
+        return this->contatos;
+    }
+    
+    std::string toString() {
+        std::stringstream ss;
+        for (auto it = this->contatos.begin(); it != this->contatos.end(); it++) {
+            ss << *it->second << std::endl;       
+        }
+        return ss.str();
+    }
+};
---
+++ arcade/agenda_map/main.cpp
@@ -0,0 +1,54 @@
+#include "agenda.hpp"
+
+int main() {
+    std::list<std::shared_ptr<Fone>> dois;
+    dois.push_back(std::make_shared<Fone>("oi", "42312"));
+    
+    std::list<std::shared_ptr<Fone>> tres;
+    tres.push_back(std::make_shared<Fone>("oi", "412442"));
+    
+    auto c1 = std::make_shared<Contato>("henok", dois);
+    c1->addFone(std::make_shared<Fone>("tim", "42212"));
+
+    auto c2 = std::make_shared<Contato>("hugo", tres);
+
+    auto c3 = std::make_shared<Contato>("ana", tres);
+    auto c4 = std::make_shared<Contato>("alice barbosa", tres);
+    auto c5 = std::make_shared<Contato>("alice almeida", tres);
+    auto c6 = std::make_shared<Contato>("zilda", tres);
+    auto c7 = std::make_shared<Contato>("julia", tres);
+
+    Agenda agenda1;
+
+    std::cout << std::endl;
+
+    agenda1.addContato(c1);
+    agenda1.addContato(c2);
+    agenda1.addContato(c3);
+    agenda1.addContato(c4);
+    agenda1.addContato(c5);
+    agenda1.addContato(c6);
+    agenda1.addContato(c7);
+
+    std::cout << std::endl;
+
+    agenda1.procura("h");
+    std::cout << std::endl;
+    
+    agenda1.getContato("h");
+    std::cout << std::endl;
+
+    agenda1.rmContato("hugo");
+    std::cout << std::endl;
+
+    agenda1.getContato("hugo");
+    std::cout << std::endl;
+
+    agenda1.rmContato("henok");
+    std::cout << std::endl;
+
+    agenda1.procura("t");
+    std::cout << std::endl;
+
+    std::cout << agenda1.toString() << std::endl;
+}
